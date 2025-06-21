07a1cdc29413f6192fe41c86d3494b1a2ce790fc
Lee Garam <agerio100@naver.com>
2025-06-21 16:28:02 +0900

    feat(site): disallow capitalized letters(A-Z) in music tags (#7)

--- src/_data/search.js
+++ src/_data/search.js
@@ -10,6 +10,32 @@ const files = fs
 
 module.exports = files.flatMap((file) => {
     const artist = path.basename(file, path.extname(file));
-    const songs = yaml.load(fs.readFileSync(path.join(musicDir, file), "utf8"));
+    const contents = fs.readFileSync(path.join(musicDir, file), "utf8");
+    const songs = yaml.load(contents);
+
+    if (!Array.isArray(songs)) {
+        throw new Error(
+            `Expected an array of songs in ${file}, got ${typeof songs}`
+        );
+    }
+
+    songs.forEach((song) => {
+        console.log(`Processing music data: ${song.title} by ${artist}`);
+        if (!Array.isArray(song.tags)) {
+            // Skip songs without tags
+            return;
+        }
+
+        // Song tags must not contain uppercase letters
+        song.tags.forEach((tag) => {
+            if (/[A-Z]/.test(tag)) {
+                throw new Error(
+                    `Invalid tag "${tag}" in file "${file}" for song "${song.title}".\n` +
+                        `→ Tags must be all lowercase for consistency.`
+                );
+            }
+        });
+    });
+
     return songs.map((song) => ({ artist, ...song }));
 });
